cb2c0a04554203a405840f795da027bc22aa1e9b
AYLINE0906 <aylinstephaniereyes@gmail.com>
2024-06-25 19:26:40 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+
+menuToggle.addEventlistener("click", () => {
+    menuToggle.classList.toggle("active");
+showcase.classList.toggle("active");
+}>
+
